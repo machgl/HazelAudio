cb769798e7a82d7ed951031285f63b7771e6fca0
James Kenny <76491611+machgl@users.noreply.github.com>
2022-03-07 19:06:37 +0000

    Added MP3 and OGG decoding

--- HazelAudio/vendor/libogg/premake5.lua
+++ HazelAudio/vendor/libogg/premake5.lua
@@ -1,7 +1,7 @@
 project "libogg"
 	kind "StaticLib"
 	language "C"
-	staticruntime "on"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
